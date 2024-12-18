e5be76c4c3a2cdf25517fe706480382626eb3f91
isalim079 <salimreza1025@gmail.com>
2024-12-18 15:18:36 +0600

    Pricing plans responsive done

--- src/pages/Home/PricingPlans/PostizPricing.jsx
+++ src/pages/Home/PricingPlans/PostizPricing.jsx
@@ -3,18 +3,18 @@ import { postizePricingData } from "./pricingData";
 
 const PostizPricing = () => {
   return (
-    <div className=" p-[44px] bg-[#1A1919] rounded-2xl">
+    <div className="p-6 lg:p-[44px] bg-[#1A1919] rounded-2xl">
       {/* title section */}
-      <div className="font-jakarta mt-[40px]">
-        <h4 className="text-[34px] font-semibold mb-[40px] text-white">
+      <div className="font-jakarta mt-7 lg:mt-[40px]">
+        <h4 className=" text-[22px] lg:text-[34px] font-semibold mb-[40px] text-white">
           Postiz Pricing
         </h4>
         <ul className="mb-6">
-          <li className="flex items-center gap-3">
+          <li className="flex items-center gap-2 lg:gap-3">
             <span className="p-2 rounded-full text-green-600 bg-[#FFFFFF0D]">
               <FaCheck />
             </span>
-            <span className="font-dmSans text-[22px] text-[#D1D1D1]">
+            <span className="font-dmSans text-[18px] lg:text-[22px] text-[#D1D1D1]">
               14 day free trial, credit card required
             </span>
           </li>
@@ -22,7 +22,7 @@ const PostizPricing = () => {
             <span className="p-2 rounded-full text-green-600 bg-[#FFFFFF0D]">
               <FaCheck />
             </span>
-            <span className="font-dmSans text-[22px] text-[#D1D1D1]">
+            <span className="font-dmSans text-[18px] lg:text-[22px] text-[#D1D1D1]">
               10% annual discount
             </span>
           </li>
@@ -34,18 +34,24 @@ const PostizPricing = () => {
         {postizePricingData.map((item) => (
           <div
             key={item.id}
-            className="flex items-center bg-[#242323] p-7 rounded-lg mb-2 text-white justify-between font-jakarta"
+            className="flex lg:flex-row flex-col space-y-4 lg:space-y-0 lg:items-center bg-[#242323] p-7 rounded-lg mb-2 text-white justify-between font-jakarta"
           >
             <div>
-              <h6 className="text-[22px] font-semibold">{item.pricingName}</h6>
-              <p className="text-[22px] font-semibold">
-                <span className="text-[40px] font-semibold">${item.price}</span>
+              <h6 className="text-[18px] lg:text-[22px] font-semibold">
+                {item.pricingName}
+              </h6>
+              <p className="text-[18px] lg:text-[22px] font-semibold">
+                <span className="lg:text-[40px] text-[32px] font-semibold">
+                  ${item.price}
+                </span>
                 /mo
               </p>
             </div>
             <div className="font-dmSans">
-              <p className="text-[20px]">Social sets: {item.socialSets}</p>
-              <p className="text-[20px]">Users: {item.users}</p>
+              <p className="text-[18px] lg:text-[20px]">
+                Social sets: {item.socialSets}
+              </p>
+              <p className="text-[18px] lg:text-[20px]">Users: {item.users}</p>
             </div>
           </div>
         ))}
--- src/pages/Home/PricingPlans/PricingPlans.jsx
+++ src/pages/Home/PricingPlans/PricingPlans.jsx
@@ -3,18 +3,18 @@ import PostizPricing from "./PostizPricing";
 
 const PricingPlans = () => {
   return (
-    <div className="lg:mt-[120px]">
+    <div className="lg:mt-[120px] mt-[60px] px-6 lg:px-0">
       <div>
         {/* Title */}
-        <div className="mb-[60px]">
-          <h1 className="font-jakarta text-[48px] font-semibold text-center text-white">
+        <div className="mb-8 lg:mb-[60px]">
+          <h1 className="font-jakarta text-[30px] lg:text-[48px] font-semibold text-center text-white">
             Pricing & Plans
           </h1>
         </div>
 
         {/* pricing cards */}
         <div className="max-w-screen-xl mx-auto">
-            <div className="grid grid-cols-2 gap-5">
+            <div className="grid lg:grid-cols-2 gap-5">
             <PostizPricing />
             <BufferPricing />
             </div>
